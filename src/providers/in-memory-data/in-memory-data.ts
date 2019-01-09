aae87735a106c7b556b7d02d622b3edbad615bd2
Zidane Wright <zidanewright@outlook.com>
2019-01-08 21:11:42 -0500

    use fakerjs to generate unique fake data

--- src/providers/in-memory-data/in-memory-data.ts
+++ src/providers/in-memory-data/in-memory-data.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { fake } from "faker";
 
 @Injectable()
 export class InMemoryDataProvider implements InMemoryDbService {
@@ -10,7 +11,21 @@ export class InMemoryDataProvider implements InMemoryDbService {
 
     for (let i = 0; i < this.MAX_EVENTS; i++) {
       events.push({
-        id: i + 1
+        id: i + 1,
+        title: fake("{{name.title}}"),
+        host: fake("{{company.companyName}}"),
+        startTime: "11:00am",
+        endTime: "11:00pm",
+        venue: fake("{{address.city}}"),
+        speaker: fake("{{name.title}}. {{name.firstName}} {{name.lastName}}"),
+        description: fake("{{lorem.paragraph}}"),
+        location: {
+          name: fake("{{company.companyName}}"),
+          coords: {
+            lat: fake("{{address.latitude}}"),
+            lon: fake("{{address.longitude}}")
+          }
+        }
       });
     }
 

1c77cc409ccf9767ee0fd728a87af1a1cb9c79bd
Nathan Downer <nathandowner123@gmail.com>
2019-01-11 23:09:01 -0500

    search works by all categories

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
+import { Component, ViewChild } from "@angular/core";
+import { NavController, Searchbar, ModalController } from "ionic-angular";
 import { EventsProvider } from "../../providers/events/events";
 import { Event } from "../../models/event";
 import { EventViewPage } from "../event-view/event-view";
@@ -9,13 +9,16 @@ import { EventViewPage } from "../event-view/event-view";
   templateUrl: "schedule.html"
 })
 export class SchedulePage {
+
+  isSearching: boolean = false;
+
   events: Event[];
   filterEvents: Event[];
   searchTerm: string;
   day1: Event[] = [];
   day2: Event[] = [];
   day3: Event[] = [];
-  day:string = "day1";
+  day:string = "all";
 
   constructor(
     private navCtrl: NavController,
@@ -24,6 +27,16 @@ export class SchedulePage {
 
   ionViewDidLoad() {
     this.getEvents();
+  }
+
+  toggleSearch() {
+    // this.modalCtrl.create(SearchPage, {
+    //   events: this.events
+    // }).present();
+    this.isSearching = !this.isSearching;
+    if (this.isSearching) {
+      // this.searchbar.setFocus();
+    }
   }
 
   showEventPage(event) {
@@ -39,12 +52,25 @@ export class SchedulePage {
     });
   }
 
+  /**
+   * 
+   * @param param 
+   * @param val 
+   */
+  inField(param:any, val: string): boolean {
+
+    return (param.toString().toLowerCase().indexOf(val.toLowerCase()) > -1)
+  }
+
+  stub(item: Event, val: string): boolean {
+    return this.inField(item.title, val) || 
+    this.inField(item.speaker, val) ||
+    this.inField(item.venue, val)
+  }
+
   findEvents() {
+    let val = this.searchTerm;
     // if the value is an empty string don't filter the items
-    this.filterEvents = this.events.filter(event => {
-      return (
-        event.title.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1
-      );
-    });
+    this.filterEvents = this.events.filter((item) => this.stub(item, val));
   }
 }

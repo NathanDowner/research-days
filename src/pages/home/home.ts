65e8ab6e8857a836359ba26593d377e995d06d76
Nathan Downer <nathandowner123@gmail.com>
2019-02-01 00:38:13 -0500

    cleaned up the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,4 +14,13 @@ export class HomePage {
   goToSchedule() {
     this.navCtrl.parent.select(3);
   }
+
+  openInsta() {
+    window.open('https://www.instagram.com', '_system');
+  }
+
+  openFb() {
+    window.open('https://www.facebook.com', '_system');
+  }
 }
+
